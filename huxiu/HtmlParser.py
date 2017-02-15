fb8832a9fea8f37adca0f5861655b8d3ebaa319e
bigsuperyu <bigsuperangel@gmail.com>
2017-02-15 22:18:38 +0800

    update

--- huxiu/HtmlParser.py
+++ huxiu/HtmlParser.py
@@ -41,67 +41,70 @@ class Html_Parser(object):
         return L
 
     def parse_detail(self):
-        f = open('detail', 'rb')
+        f = open('detail2', 'rb')
         cont = pickle.load(f)
         f.close()
         d = pq(cont)
 
-        # 处理公司信息
-        # companys = []
-        # for item in d(".box-moder ul").children('li').items():
-        #     result = item.text()
-        #     dic = {}
-        #     print(result)
-        #     if result.find('创始人')> -1:
-        #         print(result.split('：')[1])
-        #         dic['initiator']=result.split('：')[1]
-        #     if result.find('网址')> -1:
-        #         print(result.split('：')[1])
-        #         dic['companyUrl']=result.split('：')[1]
-        #     if result.find('融资')> -1:
-        #         print(result.split('：')[1])
-        #         dic['step']=result.split('：')[1]
-        #     if result.find('地址')> -1:
-        #         print(result.split('：')[1])
-        #         dic['address']=result.split('：')[1]
-        #     if result.find('员工')> -1:
-        #         print(result.split('：')[1])
-        #         dic['employeeCount']=result.split('：')[1]
-        #     if result.find('公司')> -1:
-        #         print(result.split('：')[1])
-        #         dic['companyName']=result.split('：')[1]
-        #     if result.find('法定代表')> -1:
-        #         print(result.split('：')[1])
-        #         dic['legal']=result.split('：')[1]
-        #     if result.find('公司类型')> -1:
-        #         print(result.split('：')[1])
-        #         dic['companyType']=result.split('：')[1]
-        #     if result.find('成立时间')> -1:
-        #         print(result.split('：')[1])
-        #         dic['establishDate']=result.split('：')[1]
+        pid = d("#item-id").val()
+        print(pid)
 
-        #     if result.find('注册资本')> -1:
-        #         print(result.split('：')[1])
-        #         dic['regCapital']=result.split('：')[1]
-        #     if result.find('注册时间')> -1:
-        #         print(result.split('：')[1])
-        #         dic['regDate']=result.split('：')[1]
-        #     if result.find('当前股东人数')> -1:
-        #         print(result.split('：')[1])
-        #         dic['stockholderCount']=result.split('：')[1]
-        #     if result.find('办公地点')> -1:
-        #         print(result.split(':')[1])
-        #         dic['officeAddress']=result.split('：')[1]
-        #     if result.find('联系电话')> -1:
-        #         print(result.split('：')[1])
-        #         dic['phone']=result.split('：')[1]
-        #     if result.find('电子邮箱')> -1:
-        #         print(result.split('：')[1])
-        #         dic['email']=result.split('：')[1]
-        #     companys.append(dic)
+        # 处理公司信息
+        company = {}
+        company['pid']=pid
+        for item in d(".box-moder ul").children('li').items():
+            result = item.text()
+            print(result)
+            if result.find('创始人')> -1:
+                print(result.split('：')[1])
+                company['initiator']=result.split('：')[1]
+            if result.find('网址')> -1:
+                print(result.split('：')[1])
+                company['companyUrl']=result.split('：')[1]
+            if result.find('融资')> -1:
+                print(result.split('：')[1])
+                company['step']=result.split('：')[1]
+            if result.find('地址')> -1:
+                print(result.split('：')[1])
+                company['address']=result.split('：')[1]
+            if result.find('员工')> -1:
+                print(result.split('：')[1])
+                company['employeeCount']=result.split('：')[1]
+            if result.find('公司')> -1:
+                print(result.split('：')[1])
+                company['companyName']=result.split('：')[1]
+            if result.find('法定代表')> -1:
+                print(result.split('：')[1])
+                company['legal']=result.split('：')[1]
+            if result.find('公司类型')> -1:
+                print(result.split('：')[1])
+                company['companyType']=result.split('：')[1]
+            if result.find('成立时间')> -1:
+                print(result.split('：')[1])
+                company['establishDate']=result.split('：')[1]
+
+            if result.find('注册资本')> -1:
+                print(result.split('：')[1])
+                company['regCapital']=result.split('：')[1]
+            if result.find('注册时间')> -1:
+                print(result.split('：')[1])
+                company['regDate']=result.split('：')[1]
+            if result.find('当前股东人数')> -1:
+                print(result.split('：')[1])
+                company['stockholderCount']=result.split('：')[1]
+            if result.find('办公地点')> -1:
+                print(result.split(':')[1])
+                company['officeAddress']=result.split('：')[1]
+            if result.find('联系电话')> -1:
+                print(result.split('：')[1])
+                company['phone']=result.split('：')[1]
+            if result.find('电子邮箱')> -1:
+                print(result.split('：')[1])
+                company['email']=result.split('：')[1]
+
 
         product = {}
-        imgs = []
+
         type = 1
         for x in d(".cy-cp-nav ul").children().items():
             if x.text().find('经营范围')>-1:
@@ -110,9 +113,10 @@ class Html_Parser(object):
 
         product['logo'] = d(".cy-icon-box img").attr("src")
         product['cpName'] = d(".cy-cp-name").text()
+        # 图片列表
+        imgs = []
         for item in d(".gallery-img-box").children().items():
             imgs.append(item.find('img').attr("src"))
-        print(imgs)
 
         if type == 2 :
             product['publishDate'] = d(".cy-xq-time").text().split('：')[1]
@@ -120,13 +124,35 @@ class Html_Parser(object):
             product['bizScope'] = d("#business").parent().find(".cy-cp-advantage").text()
             product['shareholders'] = d("#shareholders").parent().find(".cy-cp-advantage").text()
             product['type'] = 2
-            # product['changeRecord'] = d("#change").parent().find(".cy-cp-advantage").html()
+
+            ###### 变更table
+            changes = []
+            cols = ["sort","changeDate","changeItems","beforeChange","afterChange"]
+            for x in d(".tree-table tbody").children().items():
+                dic = {}
+                for index,td in enumerate(x.children().items()):
+                    dic[cols[index]] = td.text()
+                changes.append(dic)
+            print(changes)
+            return type,changes,company,imgs,prodcut
 
         if type == 1 :
             product['advantage'] = d("#advantage").parent().find(".cy-cp-advantage").text()
             product['results'] = d("#results").parent().find(".cy-cp-advantage").text()
 
-        print(product)
+            # 团队列表
+            teams = []
+            for x in d(".cy-cp-team").children().items():
+                dic = {}
+                dic['personName'] = x(".team-personnel-name").text()
+                dic['personPosition'] = x(".team-personnel-position").text()
+                dic['personIntro'] = x(".team-personnel-intro").text()
+                dic['pid']= pid
+                teams.append(dic)
+            print(teams)
+            return type,teams,company,imgs,prodcut
+
+        # print(product)
 
     def parse(self):
         f = open('huxiu.txt', 'rb')
